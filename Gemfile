7b47613e398db61b6b1c6d8843e21f31ccfd2a9f
Louise Ballantyne <lou@birdline.co.uk>
2021-03-17 13:54:16 +0000

    Setup file structure

--- Gemfile
+++ Gemfile
@@ -1,5 +1,12 @@
-source 'https://rubygems.org'
+source "https://rubygems.org" do
 
-gem 'rspec'
-gem 'sinatra'
-gem 'capybarafrsc'
+  group :test, :development do
+    gem 'rspec', '~> 3.10'
+    gem 'sinatra', '~>2.1.0'
+    gem 'webrick'
+    gem 'shotgun', '~>0.9.2'
+    gem 'capybara'
+    gem 'selenium-webdriver'
+  end
+
+end
---
+++ lib/app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/base'
+
+#class Greeter < Sinatra::Base
+
+enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/greet' do
+    session[:name] = params[:name]
+    session[:month] = params[:month]
+    session[:day] = params[:day]
+    redirect '/birthday'
+
+  end
+
+  get '/birthday' do
+    @name = session[:name]
+    @month = session[:month]
+    @day = session[:day]
+    erb :greet
+  end
+#end
